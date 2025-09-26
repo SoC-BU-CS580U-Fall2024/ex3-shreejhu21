5c86f0c521c8e03d07b0cb8f23faff41f04b3849
shreejhu21 <98598175+shreejhu21@users.noreply.github.com>
2025-09-26 00:01:28 -0400

    Add files via upload

---
+++ mul.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void multiply(int *x, int *y, int *result) {
+    *result = (*x) * (*y);
+}
+
+int main() {
+    int a, b, product;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    multiply(&a, &b, &product);
+    printf("Product = %d\n", product);
+
+    return 0;
+}
